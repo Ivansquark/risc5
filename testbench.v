2104c2deff577280baf976ed3d72ad83478a88a7
Ivansquark <squark@mail.ru>
2021-11-04 20:56:03 +0300

    +icarus testbench

--- testbench.v
+++ testbench.v
@@ -1,16 +1,22 @@
-'timescale 1 ns / 100 ps
+`timescale 1 ns / 100 ps
 
-module testbench();
 
-reg clk = 1`b0;
+module testbench(); /* No inputs, no outputs */
+
+reg clk = 1'b0; /* Represents clock, initial value is 0 */
+
 always begin
-	#1 clk <= ~clk;
+    #1 clk = ~clk; /* Toggle clock every 1ns */
 end
 
-wire clk1;
+wire clk1; /* For output of tested module */
 
-rv_alu rv_alu(clk(clk), clk1(clk1));
+rv_alu rv_alu(.clkin(clk), .clkout(clk1)); /* Tested module instance */
 
+initial begin
+    $dumpvars;      /* Open for dump of signals */
+    $display("Test started...");   /* Write to console */
+    #10 $finish;    /* Stop simulation after 10ns */
+end
 
-
-endmodule 
+endmodule
