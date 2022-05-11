e4a2e155def7215e43d0f2bc49aa1bb67ac101bb
iv3505703 <iv3505703@gmail.com>
2022-05-11 21:30:52 +0300

    + simple rom(flash) for altera

---
+++ rom.v
@@ -0,0 +1,17 @@
+module rom #(
+parameter LENGTH = 1024, 
+parameter WIDTH = 32) // - flash size in bits (= 4KB)
+(
+    input  [WIDTH - 1 : 0] address;    // Program Counter
+    output [WIDTH - 1 : 0] instruction; 
+);
+
+reg [WIDTH - 1 : 0] mem[LENGTH - 1 : 0]; //array of registers
+
+assign instruction = mem[address]; //address must be aligned on 4
+
+initial begin
+    $readmemh(program.txt, mem); //fill rom (for altera)
+end
+
+endmodule
