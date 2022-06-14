9c441391e1bbe18553a34d69c87b91b4558b6c34
iv3505703 <iv3505703@gmail.com>
2022-06-14 22:35:30 +0300

    add c program

--- program/c/blink/lib/startup.c
+++ program/c/blink/lib/startup.c
@@ -1,7 +1,9 @@
 void _start();
 
-void (*mainPtr)(void) = _start;
+//void (*main)(void) = _start;
 
+    asm("add ra, zero,ra");
+    asm("la sp, _estack");
 extern void* _estack;
 extern void* _sidata, *_sdata, *_edata, *_sbss, *_ebss;
 void _start() {
@@ -14,7 +16,11 @@ void _start() {
     for(pDst = &_sbss; pDst != &_ebss; pDst++) {
         *pDst = 0;
     }
-
+    /* Clear the link register, so debuggers know to terminate their
+     * backtrace. */
+    //cfi_undefined(ra);
+    /* Setup the stack pointer.  This symbol will only be looked at
+     * here, so you can pick any name you want. */
     main();
     while(1);
 }
